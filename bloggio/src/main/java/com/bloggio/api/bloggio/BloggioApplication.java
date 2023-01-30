8a7f3165944c7e2d6867f25829f3441c673b0363
PieroRafael <pierobecerra19@gmail.com>
2023-01-29 22:45:05 -0500

    Inital Commit

---
+++ bloggio/src/main/java/com/bloggio/api/bloggio/BloggioApplication.java
@@ -0,0 +1,13 @@
+package com.bloggio.api.bloggio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggioApplication.class, args);
+	}
+
+}
---
+++ bloggio/src/test/java/com/bloggio/api/bloggio/BloggioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloggio.api.bloggio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
