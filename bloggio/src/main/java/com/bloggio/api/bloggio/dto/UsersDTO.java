edc9aa253f865c6796c75edbb39e9d51cb959db0
PieroRafael <pierobecerra19@gmail.com>
2023-02-01 11:37:50 -0500

    DTO Users

---
+++ bloggio/src/main/java/com/bloggio/api/bloggio/dto/UsersDTO.java
@@ -0,0 +1,26 @@
+package com.bloggio.api.bloggio.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String userNickname;
+
+    private String userPassword;
+
+    private String userPhoto;
+
+    private String userShortBio;
+
+}
