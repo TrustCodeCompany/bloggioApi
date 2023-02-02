2da43181153d6b128b580e036a9b70a381e3b8d2
PieroRafael <pierobecerra19@gmail.com>
2023-02-01 21:06:08 -0500

    First Enjoy

--- bloggio/src/main/java/com/bloggio/api/bloggio/controller/UsersController.java
+++ bloggio/src/main/java/com/bloggio/api/bloggio/controller/UsersController.java
@@ -1,8 +1,11 @@
 package com.bloggio.api.bloggio.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bloggio.api.bloggio.dto.UsersDTO;
 import com.bloggio.api.bloggio.service.UsersService;
 
 @RestController
@@ -13,6 +16,11 @@ public class UsersController {
 
     public UsersController(UsersService usersService) {
         this.usersService = usersService;
+    }
+
+    @PostMapping("/Create")
+    public UsersDTO Create(@RequestBody UsersDTO usersDTO) {
+        return usersService.create(usersDTO);
     }
 
 }
--- bloggio/src/main/java/com/bloggio/api/bloggio/service/UsersService.java
+++ bloggio/src/main/java/com/bloggio/api/bloggio/service/UsersService.java
@@ -2,15 +2,25 @@ package com.bloggio.api.bloggio.service;
 
 import org.springframework.stereotype.Service;
 
+import com.bloggio.api.bloggio.dto.UsersDTO;
+import com.bloggio.api.bloggio.mapper.UsersMapperImpl;
+import com.bloggio.api.bloggio.persistence.entity.Users;
 import com.bloggio.api.bloggio.persistence.repository.UsersRepository;
 
 @Service
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final UsersMapperImpl usersMapperImpl;
 
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, UsersMapperImpl usersMapperImpl) {
         this.usersRepository = usersRepository;
+        this.usersMapperImpl = usersMapperImpl;
+    }
+
+    public UsersDTO create(UsersDTO usersDTO) {
+        Users users = usersMapperImpl.usersDTOToUsers(usersDTO);
+        return usersMapperImpl.usersToUsersDTO(usersRepository.save(users));
     }
 
 }
