36cc3bcf3183e0aad566e0d8a05fa7566db1f928
rricardob <ricardo_23_91@hotmail.com>
2023-03-06 19:32:49 -0500

    fix errors

--- bloggio/src/main/java/com/bloggio/api/bloggio/security/jwt/JwtUtils.java
+++ bloggio/src/main/java/com/bloggio/api/bloggio/security/jwt/JwtUtils.java
@@ -17,10 +17,10 @@ public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${bezkoder.app.jwtSecret}")
+    @Value("${bloggio.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${bezkoder.app.jwtExpirationMs}")
+    @Value("${bloggio.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
