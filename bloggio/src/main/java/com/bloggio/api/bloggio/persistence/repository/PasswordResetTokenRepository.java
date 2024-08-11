202705dc641543ed7020adbaea06e911cef9e4d3
Piero Rafael <pierobecerra19@gmail.com>
2024-08-11 11:39:27 -0500

    1.New Endpoint Change Password , 2. New Endpoint Recovery Password , 3. New Service Send Emails , 4. Validation Add Profile User

---
+++ bloggio/src/main/java/com/bloggio/api/bloggio/persistence/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,14 @@
+package com.bloggio.api.bloggio.persistence.repository;
+
+import com.bloggio.api.bloggio.persistence.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken,Long> {
+
+     Optional <PasswordResetToken> findByToken(String token);
+
+}
---
+++ bloggio/src/main/java/com/bloggio/api/bloggio/service/EmailService.java
@@ -0,0 +1,47 @@
+package com.bloggio.api.bloggio.service;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class EmailService {
+
+    @Value("${sendgrid.api-key}")
+    private String apiKey;
+
+    public EmailService() {}
+
+    public void sendEmail (String to, String subject, String content) throws IOException {
+
+        Email from = new Email("trustcodesac@gmail.com");
+        Email toEmail = new Email(to);
+        Content emailContent = new Content("text/plain", content);
+        Mail mail = new Mail(from,subject,toEmail,emailContent);
+
+        SendGrid sg = new SendGrid(apiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        }catch (IOException ex){
+            throw ex;
+        }
+
+    }
+
+}
